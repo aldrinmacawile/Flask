db42874475bdcdf462146498d38365a3780764f5
Aldrin Macawile <Aeyemmmacawile@gmail.com>
2021-11-30 20:16:19 +0800

    commit

--- coop.py
+++ coop.py
@@ -1,27 +1,42 @@
+from enum import unique
 from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms import validators
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 #Create a flask instance
 app = Flask(__name__)
+#Add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#Secret Key!
 app.config['SECRET_KEY'] = "Goop123"
 
+#Initialize the database
+db = SQLAlchemy(app)
+#Create a model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    #Create a String
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 # Create a form Class
-class Namerform(FlaskForm):
+class Userrform(FlaskForm):
     name = StringField("Name:", validators=[DataRequired()])
+    email = StringField("Email:", validators=[DataRequired()])
     submit = SubmitField("Submit")
-
-
 
 
 #Create a route decorator
 @app.route('/')
-
-#def index():
-    #return "<h1>Hello World!</h1>"
 
 #safe, capitalize, lower, upper, title, trim, striptags,   -- jinja filters
 
@@ -29,19 +44,23 @@ def index():
     return render_template("index.html")
 
 #localhost5000:/user/macawile
-@app.route('/register', methods=['GET', 'POST'])
-
+@app.route('/register/add', methods=['GET', 'POST'])
 def register():
-    #return "<h1>Hello {}!!!</h1>".format(name)
     name = None
-    form = Namerform()
+    form = Userrform()
     #validators
     if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
         name = form.name.data
         form.name.data = ''
-        flash("Form Submitted Succesfully Please Login!")
-
-    return render_template("register.html", name = name, form = form)
+        form.email.data = ''
+        flash("User Registered Succesfully Please Login!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("register.html", name = name, form = form, our_users = our_users)
 
 # Create Custom Error pages
 # Create Invalid Url
